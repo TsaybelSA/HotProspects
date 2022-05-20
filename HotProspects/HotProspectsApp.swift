6050b25ddc083c589ed3d3c07e69e8157eecaa0b
TsaybelSA <tsaybelsa@gmail.com>
2022-05-20 14:24:08 +0600

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Сергей Цайбель on 20.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
